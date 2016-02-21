bf02a54328a3f8809cae8b345088cfb04338aab5
Nathan Adams <adamsna@datanethost.net>
2016-02-21 17:37:29 -0600

    updating init script

--- web/foreveralone.php
+++ web/foreveralone.php
@@ -22,10 +22,8 @@ if (count($argv) == 1) {
 
 switch ($argv[1]) {
 	case "all-clear":
-		\vendor\DB\DB::query("DELETE FROM histories");
 		\vendor\DB\DB::query("DELETE FROM users");
 		\vendor\DB\DB::query("DELETE FROM sessions");
-		\vendor\DB\DB::query("DELETE FROM jobs");
 		break;
 	case "adduser":
 		$user = $argv[2];
